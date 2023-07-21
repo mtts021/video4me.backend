0f6d2016b03b1e1332bfd3f298712333ca7de6c3
matheusgb <matheusgbr98@gmail.com>
2023-07-21 13:15:54 -0300

    feat: initialize user endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { ConfigModule } from '@nestjs/config'
 import { MongooseModule } from '@nestjs/mongoose'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { UsersModule } from './users/users.module'
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_URL),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/users/users.controller.ts
@@ -0,0 +1,31 @@
+import { Controller, Get, Post, Patch, Param, Delete } from '@nestjs/common'
+// import { CreateUserDto } from './dto/create-user.dto'
+// import { UpdateUserDto } from './dto/update-user.dto'
+import { UsersService } from './users.service'
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {
+    console.log('UsersController constructor')
+  }
+
+  @Post()
+  // create(@Body() createUserDto: CreateUserDto) {
+  //   return this.usersService.create(createUserDto)
+  // }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.usersService.findOne(+id)
+  }
+
+  @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  //   return this.usersService.update(+id, updateUserDto)
+  // }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.usersService.remove(+id)
+  }
+}
