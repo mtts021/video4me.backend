2700056d303fccbe6d363bf15079c97c53cf47fa
matheusgb <matheusgbr98@gmail.com>
2023-07-26 18:42:26 -0300

    test: creates the first test version for user creation in services

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,20 +1,24 @@
 import { getModelToken } from '@nestjs/mongoose'
 import { Test, TestingModule } from '@nestjs/testing'
+import { userMock, userMockReturn } from './test-utils/service-mocks'
 import { UsersService } from './users.service'
+
+class MockRepository {
+  constructor(private data) {}
+
+  create = jest.fn().mockResolvedValue(this.data).mockReturnValue(userMockReturn)
+
+  save = jest.fn().mockResolvedValue(this.data).mockReturnValue(userMockReturn)
+}
 
 describe('UsersService', () => {
   let service: UsersService
 
-  const mockRepository = {
-    create: jest.fn(),
-    save: jest.fn(),
-  }
-
-  beforeAll(async () => {
+  beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         UsersService,
-        { provide: getModelToken('Users'), useValue: mockRepository },
+        { provide: getModelToken('Users'), useValue: MockRepository },
       ],
     }).compile()
 
@@ -23,5 +27,23 @@ describe('UsersService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined()
+  })
+
+  describe('create', () => {
+    it('should create a user', async () => {
+      const createSpy = jest.spyOn(service, 'create')
+      const createUser = await service.create(userMock)
+
+      expect(createUser).toEqual(userMockReturn)
+      expect(createUser).toMatchObject(userMockReturn)
+      expect(createUser.uuid).toBeDefined()
+      expect(createUser.createdAt).toBeDefined()
+      expect(createUser.updatedAt).not.toBeDefined()
+      expect(createUser.name).toBeDefined()
+      expect(createUser.email).toBeDefined()
+      expect(createSpy).toBeCalledWith(userMock)
+      expect(createSpy).toHaveBeenCalled()
+      expect(createSpy).toBeCalledTimes(1)
+    })
   })
 })
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,31 +4,29 @@ import { Model } from 'mongoose'
 import { v4 as uuidv4 } from 'uuid'
 import { CreateUserDTO } from './dtos/create-user.dto'
 import { UserReturn } from './interfaces/user-return.interface'
-import { User } from './schemas/user.schema'
+import { User, UserDocument } from './schemas/user.schema'
 import { encodePassword } from './utils/bcrypt'
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectModel('Users') private readonly UserModel: Model<User>) {}
+  constructor(@InjectModel('Users') private readonly UserModel: Model<UserDocument>) {}
 
   async create(user: CreateUserDTO): Promise<UserReturn> {
     const encodedPassword = await encodePassword(user.password)
-    const createdUser = new this.UserModel(
-      {
-        uuid: uuidv4(),
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        ...user,
-        password: encodedPassword,
-      },
-    )
-    createdUser.save()
+    const createdUser = new this.UserModel({
+      uuid: uuidv4(),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      ...user,
+      password: encodedPassword,
+    })
+    const userCreated = await createdUser.save()
 
     const returnUser: UserReturn = {
-      uuid: createdUser.uuid,
-      name: createdUser.name,
-      email: createdUser.email,
-      createdAt: createdUser.createdAt,
+      uuid: userCreated.uuid,
+      name: userCreated.name,
+      email: userCreated.email,
+      createdAt: userCreated.createdAt,
     }
 
     return returnUser
